bd18677a97648663b820203d94e8327222cf09ba
James Ryan <jamesryan@Jamess-MBP-2.fios-router.home>
2019-10-30 00:55:32 -0400

    switching to sqlite3 calls; ensure files saving in vscode

--- db_handler.py
+++ db_handler.py
@@ -1,21 +1,28 @@
 import sqlite3
 
 
-def init_db():
+def init_db(): # make the database for the first time
     conn = sqlite3.connect('steven.sqlite3')
     cur = conn.cursor()
-    cur.execute('''CREATE TABLE entries (inbound TEXT, command TEXT, item TEXT, price INTEGER)''')
+    cur.execute('''CREATE TABLE entries (inbound TEXT, command TEXT, item TEXT, price INTEGER)''') # make entries table
     conn.commit()
     conn.close()
 
 
 def add_entry(params):
-    conn = sqlite3.connect('steven.sqlite3')
-    cur = conn.cursor()
-    cur.execute('INSERT INTO entries VALUES (?, ? , ?, ?)', params)
-    cur.execute('SELECT * FROM entries')
+    conn = sqlite3.connect('steven.sqlite3') # connect to steven database
+    cur = conn.cursor() # cursor object to maneuver/manipulate database
+    cur.execute('INSERT INTO entries VALUES (?, ? , ?, ?)', params) 
+    cur.execute('SELECT * FROM entries') # in order to print/fetchall()
     print(cur.fetchall())
     conn.commit()
+    conn.close()
+
+def get_all_entries():
+    conn = sqlite3.connect('steven.sqlite3') # connect to steven database
+    cur = conn.cursor() # cursor object to maneuver/manipulate database
+    cur.execute('SELECT * FROM entries') # in order to print/fetchall()
+    print(type(cur.fetchall()))
     conn.close()
 
 
--- steven.py
+++ steven.py
@@ -1,45 +1,38 @@
-import pickle
-
-shopping = {}
+import db_handler as db # making calls to db based on commans
 
 
 def handle(params):
-    global shopping
-    pickle_in = open("shopping.p", "rb")
-    shopping = pickle.load(pickle_in)
-    res = 'Error: invalid command'
+    res = 'Error: invalid command or formatting'
     cmd = params[1].lower()
     if cmd == 'add':
-        res = add_item(params[2], params[3])
+        #res = add_item(params[2], params[3])
+        res = db.add_item(params)
     elif cmd == 'list':
-        res = gen_list_response()
-    elif cmd == 'bought':
+        res = db.get_all_entries()
+    elif cmd == 'bought': # not yet converted to DB call
         res = remove_item(params[1], int(params[2]))
-    pickle_out = open("shopping.p", "wb")
-    pickle.dump(shopping, pickle_out)
-    pickle_out.close()
     return res
 
 
-def add_item(item, cnt):
-    if item in shopping.keys():
-        shopping[item] += int(cnt)
-    else:
-        shopping[item] = int(cnt)
-    return gen_list_response()
+# def add_item(item, cnt):
+#     if item in shopping.keys():
+#         shopping[item] += int(cnt)
+#     else:
+#         shopping[item] = int(cnt)
+#     return gen_list_response()
 
 
-def gen_list_response():
-    return str(shopping)
+# def gen_list_response():
+#     return str(shopping)
 
 
-def remove_item(item, cnt):
-    if item in shopping:
-        if cnt >= shopping[item]:
-            del shopping[item]
-        else:
-            shopping[item] -= cnt
-        return gen_list_response()
-    else:
-        return "No such item in list"
+# def remove_item(item, cnt):
+#     if item in shopping:
+#         if cnt >= shopping[item]:
+#             del shopping[item]
+#         else:
+#             shopping[item] -= cnt
+#         return gen_list_response()
+#     else:
+#         return "No such item in list"
 
